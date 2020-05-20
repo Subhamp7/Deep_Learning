f80a4086a7645c43b44d2d88948644866989a6cb
Subhamp7 <subham306952@gmail.com>
2020-05-21 00:29:05 +0530

    Adding the final ANN code

--- ANN/ANN_Model.py
+++ ANN/ANN_Model.py
@@ -28,18 +28,34 @@ LabelEncoder_X_2=LabelEncoder()
 X[:,2]=LabelEncoder_X_2.fit_transform(X[:,2])
 onehotencoder=ColumnTransformer([('one_hot', OneHotEncoder(),[1])], remainder='passthrough')
 X= np.array(onehotencoder.fit_transform(X), dtype=np.float)
+X=X[:,1:]
 
 #splitting the dataset to test and train data
 X_train, X_test, Y_train, Y_test=train_test_split(X, Y, test_size=0.2, random_state=0)
 
-#scaling the test and train data
+#Feature scaling 
 sc=StandardScaler()
 X_test=sc.fit_transform(X_test)
 X_train=sc.transform(X_train)
 
+classifier= Sequential()
 
+#adding first hidden layer
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu' ,input_dim=11 ))
+#adding second hidden layer
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+#adding the output layer
+classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+#compiling the ANN
+classifier.compile(optimizer ='adam', loss='binary_crossentropy', metrics=['accuracy'])
+ #fitting the model
+classifier.fit(X_train, Y_train, batch_size=10, epochs=100 )
+#predicting the test data
+Y_pred=classifier.predict(X_test)
+Y_pred=(Y_pred>0.5)
 
-
-
-
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(Y_test, Y_pred)
+print('confusion Matrix :',cm)
+    
 print('Completed')
