1fc2992a2b703887a838d658530c0eb4aa1428d1
Subhamp7 <subham306952@gmail.com>
2020-05-21 21:11:30 +0530

    Tunning the model

--- ANN/ANN_Model.py
+++ ANN/ANN_Model.py
@@ -7,14 +7,17 @@ Created on Tue May 19 20:43:54 2020
 #importing libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import keras
 from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+
 
 #importing dataset
 dataset=pd.read_csv('Churn_Modelling.csv')
@@ -38,8 +41,8 @@ sc=StandardScaler()
 X_test=sc.fit_transform(X_test)
 X_train=sc.transform(X_train)
 
-'''classifier= Sequential()
-
+# Initialising the ANN
+classifier= Sequential()
 #adding first hidden layer
 classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu' ,input_dim=11 ))
 #adding second hidden layer
@@ -53,15 +56,11 @@ classifier.fit(X_train, Y_train, batch_size=10, epochs=100 )
 #predicting the test data
 Y_pred=classifier.predict(X_test)
 Y_pred=(Y_pred>0.5)
-
-from sklearn.metrics import confusion_matrix
+#Confusion Matrix
 cm=confusion_matrix(Y_test, Y_pred)
-print('confusion Matrix :',cm)
-'''
+print('confusion Matrix for the ANN model:',cm)
 
-from keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.model_selection import cross_val_score
-
+# Evaluating the ANN
 def build_classifier():
     classifier= Sequential()
     classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu' ,input_dim=11 ))
@@ -69,10 +68,28 @@ def build_classifier():
     classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
     classifier.compile(optimizer ='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return classifier
-
 classifier=KerasClassifier(build_fn=build_classifier,batch_size=10, epochs=100 )
-accuracies=cross_val_score(estimator= classifier, X=X_train, y=Y_train, cv= 10, n_jobs=-1)
+accuracies=cross_val_score(estimator= classifier, X=X_train, y=Y_train, cv= 10, n_jobs=-1) #n_jobs=-1 for using all processors
+mean = accuracies.mean()
+variance = accuracies.std()
 
-
-    
+# Tuning the ANN
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier)
+parameters = {'batch_size': [25, 32],
+              'epochs': [100, 500],
+              'optimizer': ['adam', 'rmsprop']}
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10)
+grid_search = grid_search.fit(X_train, Y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
 print('Completed')
