296f8881ae868f4fa33d5d88a8f3fda428405159
Subhamp7 <subham306952@gmail.com>
2020-06-02 16:54:23 +0530

    completed som code

--- SOM/SOM_DL.py
+++ SOM/SOM_DL.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 29 11:57:03 2020
-
-@author: subham
-"""
 #importing the required libararies
 import numpy as np
 import pandas as pd
@@ -27,7 +21,32 @@ X=sc.fit_transform(X)
 
 #training the SOM(Using Minisom.py)
 som=MiniSom(x=10, y=10,input_len=15, sigma=1.0, learning_rate=0.5)
+som.random_weights_init(X)
+som.train_random(data=X, num_iteration=100)
 
+#visualizing the result
+from pylab import bone, pcolor , colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers= ['o', 's']
+colors= ['r', 'g']
+for i, x in enumerate(X):
+    w= som.winner(x)
+    plot(w[0]+0.5,
+        w[1]+0.5,
+        markers[Y[i]],
+        markeredgecolor = colors[Y[i]],
+        markerfacecolor = 'None',
+        markersize= 10,
+        markeredgewidth=2)
+show()
+    
+#finding the frauds
+mappings = som.win_map(X)
+frauds= np.concatenate((mappings[(8,6)], mappings[(6,8)]))
+frauds=sc.inverse_transform(frauds)
 
+#completion 
 print('Complete')
 sound(5)
